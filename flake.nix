c00c2b72988a7e253f5f7f45642f07fab00e6e59
piyush-1337 <piyushkatkar9421@gmail.com>
2026-04-02 18:59:59 +0530

    setup tun interface

---
+++ flake.nix
@@ -0,0 +1,64 @@
+{
+  description = "A tcp stack implementation";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    crane.url = "github:ipetkov/crane";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      crane,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        craneLib = crane.mkLib pkgs;
+
+        src = craneLib.cleanCargoSource ./.;
+
+        commonArgs = {
+          inherit src;
+          strictDeps = true;
+        };
+
+        cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+
+        synoxide = craneLib.buildPackage (
+          commonArgs
+          // {
+            inherit cargoArtifacts;
+          }
+        );
+
+      in
+      {
+        packages.default = synoxide;
+
+        checks = {
+          my-crate-tests = craneLib.cargoTest (
+            commonArgs
+            // {
+              inherit cargoArtifacts;
+            }
+          );
+        };
+
+        devShells.default = craneLib.devShell {
+          packages = with pkgs; [
+            rust-analyzer
+            rustfmt
+            clippy
+          ];
+
+          RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}";
+        };
+      }
+    );
+}
---
+++ src/main.rs
@@ -0,0 +1,28 @@
+use std::io::Read;
+
+use tun::Configuration;
+
+fn main() {
+    let mut config = Configuration::default();
+
+    config
+        .tun_name("tun0")
+        .address((10, 0, 0, 1))
+        .netmask((255, 255, 255, 0))
+        .up();
+
+    config.platform_config(|config| {
+        config.ensure_root_privileges(true);
+    });
+
+    let mut dev = tun::create(&config).expect("Failed to create TUN device");
+
+    let mut buf = [0u8; 1504];
+
+    loop {
+        let n = dev.read(&mut buf).expect("Failed to read from device");
+        let packet = &buf[..n];
+
+        println!("Received {} bytes", packet.len());
+    }
+}
